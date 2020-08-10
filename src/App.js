a5e8ff70f09e82c542654b01c8401ae4c89ed69a
ShahinGhanei94 <shahinghanei94@sh94gh.local>
2020-08-10 15:35:58 +0200

    fetched the jobs from github jobs API made search box and location and full time jobs checkbox and pagination

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useFetchJobs } from './fetchJobs';
+import Job from './components/Job';
+import { Spinner } from 'react-bootstrap';
+import JobsPagination from './components/JobsPagination';
+import SearchForm from './components/SearchForm';
 
-function App() {
+const App = () => {
+  const [params, setParams] = useState({});
+  const [page, setPage] = useState(1);
+
+  const handleParamChange = (e) => {
+    const param = e.target.name;
+    const value = e.target.value;
+    setPage(1);
+    setParams((prevParams) => {
+      return { ...prevParams, [param]: value };
+    });
+  };
+
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="container my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && (
+        <div
+          style={{ height: '500px' }}
+          className="d-flex justify-content-center align-items-center"
         >
-          Learn React
-        </a>
-      </header>
+          <Spinner animation="grow" variant="primary" />{' '}
+        </div>
+      )}
+      {error && <h1>something went wrong!</h1>}
+      {jobs.map((job) => (
+        <Job key={job.id} job={job} />
+      ))}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </div>
   );
-}
+};
 
 export default App;
